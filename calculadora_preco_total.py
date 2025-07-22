eb41f0369cf351acb10437fa3e9dd9e6981e4108
VeruscaSilva-Docs <veruscaschimmels46@gmail.com>
2025-07-22 20:53:50 -0300

    Update calculadora_preco_total.py

--- calculadora_preco_total.py
+++ calculadora_preco_total.py
@@ -1,5 +1,7 @@
 # Calculadora de Preço Total
 
+# Calculadora de Preço Total
+
 # Informações do produto
 nome_produto = "Cadeira Infantil"
 preco_unitario = 12.40
@@ -13,21 +15,7 @@ print("Produto:", nome_produto)
 print("Preço unitário: R$", preco_unitario)
 print("Quantidade:", quantidade)
 print("Preço total: R$", round(preco_total, 2))
-Saída esperada:
-yaml
-Copiar
-Editar
-Produto: Cadeira Infantil
-Preço unitário: R$ 12.4
-Quantidade: 3
-Preço total: R$ 37.2
-Se quiser formatar o preço com duas casas decimais fixas, pode usar:
-
-python
-Copiar
-Editar
 print("Preço total: R$ {:.2f}".format(preco_total))
-Quer que eu mostre isso em outra linguagem também (como JavaScript ou C)?
 
 
 
